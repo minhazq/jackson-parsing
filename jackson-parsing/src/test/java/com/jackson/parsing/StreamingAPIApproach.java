79bcd4f6eb2dce9144ed00f30f80b2b2f62592d2
unknown <minhazq@gmail.com>
2016-08-20 17:07:50 -0400

    update

--- jackson-parsing/src/test/java/com/jackson/parsing/StreamingAPIApproach.java
+++ jackson-parsing/src/test/java/com/jackson/parsing/StreamingAPIApproach.java
@@ -2,34 +2,25 @@ package com.jackson.parsing;
 
 import java.io.File;
 import java.io.IOException;
-
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.JsonToken;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class StreamingAPIApproach {
 
 	public static void main(String[] args) throws JsonProcessingException, IOException {
-		
-		//Creating an object of ObjectMapper class. This is an entry point
-		ObjectMapper mapper = new ObjectMapper();
-		JsonNode jNode = mapper.readTree(new File("D:\\eclipseqaworkspace\\jackson-parsing\\src\\test\\resources\\user.json"));
-
-		
 		//Using Streaming api approach	
-				JsonFactory jFactory = new JsonFactory();
-				JsonParser jParser = jFactory.createParser(new File("D:\\eclipseqaworkspace\\jackson-parsing\\src\\test\\resources\\user.json"));
-				while(jParser.nextToken()!=JsonToken.END_OBJECT){
-					String fieldName = jParser.getCurrentName();
-					//if(fieldName.equals("name")){ ==> Not working cz looping time fieldName might be null and trying to perform equal on null
-					if("first".equals(fieldName)){
-						jParser.nextToken();
-						System.out.println(jParser.getText());
-					}
-				}
+		JsonFactory jFactory = new JsonFactory();
+		JsonParser jParser = jFactory.createParser(new File("D:\\eclipseqaworkspace\\jackson-parsing\\src\\test\\resources\\user.json"));
+		while(jParser.nextToken()!=JsonToken.END_OBJECT){
+			String fieldName = jParser.getCurrentName();
+			//if(fieldName.equals("name")){ ==> Not working cz looping time fieldName might be null and trying to perform equal on null
+			if("first".equals(fieldName)){
+				jParser.nextToken();
+				System.out.println(jParser.getText());
+			}
+		}
 
 	}
 
--- jackson-parsing/src/test/java/com/jackson/parsing/TreeModelApproach.java
+++ jackson-parsing/src/test/java/com/jackson/parsing/TreeModelApproach.java
@@ -16,40 +16,33 @@ public class TreeModelApproach {
 	public static void main(String[] args) throws JsonProcessingException, IOException {
 
 		//Using Tree Model approach
-		ObjectMapper mapper1 = new ObjectMapper();
-		JsonNode jNode = mapper1.readTree(new File("D:\\eclipseqaworkspace\\jackson-parsing\\src\\test\\resources\\user.json"));
-
-		int id = jNode.path("id").asInt();
-		String first = jNode.path("name").path("first").asText();
-
-		System.out.println(id);
-		System.out.println(first);
-
-
 		ObjectMapper mapper = new ObjectMapper();
 		JsonNode rootNode = mapper.readTree(new File("D:\\eclipseqaworkspace\\jackson-parsing02\\src\\test\\resources\\productsearch.json"));
 		//String firstName = rootNode.path("name").path("first").asText();
 		//System.out.println(firstName);
 
-
+		/*
 		//giving me the first object/element
 		Iterator<JsonNode> it = rootNode.elements();
 		while(it.hasNext()){
 			System.out.println(it.next());
-		}
+		}*/
 
 
-
-		//Giving me only tag or field name of the first element
-		Iterator<String> fieldsit = rootNode.fieldNames();
+		//Giving me only tag or field name of the first element. It will not give me the child or descendent element.
+		//In this case it is returning one field which is response. If you want all desired nodes then use findparents.
+		/*Iterator<String> fieldsit = rootNode.fieldNames();
 		while(fieldsit.hasNext()){
 			System.out.println(fieldsit.next());
-		}
+		}*/
 
-		List<JsonNode> listOfNodes = rootNode.findParents("image");
+		//giving me the all the nodes which has imageId. 
+		List<JsonNode> listOfNodes = rootNode.findParents("imageId");
 		System.out.println(listOfNodes.size());
 		for(JsonNode n : listOfNodes){
-			System.out.println(n.findValue("height").asText());
+				System.out.println(n.findValue("imageId").asText());
+			}
+			
 		}
 
 
