9bf274ab85722eddb2f692dc1d62e63abd98273c
unknown <minhazq@gmail.com>
2016-08-21 10:53:14 -0400

    update

--- jackson-parsing/src/test/java/com/jackson/parsing/StreamingAPIApproach.java
+++ jackson-parsing/src/test/java/com/jackson/parsing/StreamingAPIApproach.java
@@ -6,20 +6,31 @@ import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.TreeNode;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class StreamingAPIApproach {
 
 	public static void main(String[] args) throws JsonProcessingException, IOException {
 		//Using Streaming api approach	
-		JsonFactory jFactory = new JsonFactory();
-		JsonParser jParser = jFactory.createParser(new File("D:\\eclipseqaworkspace\\jackson-parsing\\src\\test\\resources\\user.json"));
+		
+		JsonFactory jFactory = new JsonFactory(); 
+		JsonParser jParser = jFactory.createParser(new File("D:\\eclipseqaworkspace\\github_projects\\jackson-parsing\\jackson-parsing\\src\\test\\resources\\productsearch.json"));
+		
+		
 		while(jParser.nextToken()!=JsonToken.END_OBJECT){
 			String fieldName = jParser.getCurrentName();
 			//if(fieldName.equals("name")){ ==> Not working cz looping time fieldName might be null and trying to perform equal on null
-			if("first".equals(fieldName)){
+			if("errors".equals(fieldName)){
 				jParser.nextToken();
 				System.out.println(jParser.getText());
 			}
+		
+			
+			
+			//jParser.nextToken();
+			//System.out.println(jParser.getCurrentName());
 		}
 
 	}
